65172cf4f9f99becaa9130985f9ab82bce7a3253
Xurshidbek <tmz@MacBook-Air-tmz.home>
2024-11-04 16:44:16 +0500

    Yangi project v-1

---
+++ app.js
@@ -0,0 +1,47 @@
+let Number = "28"
+let sum = 28
+let Name = "Abbos"
+// typeof ==> metod /// vazifasi ma'lumot turini aniqlash
+
+// if(Number <= sum )// false or 0 bu shartni tashlab keyingi shartga o'tadi{
+//     console.log(`Bu son ${Number} string number 28 ga teng`); 
+// }else{
+//     console.log(`Error`);
+    
+// }
+
+// || or  , && and
+
+// if((typeof Number == typeof sum ) && (typeof Name == typeof Number))// false + true== 0+1// 0 * 1=0
+//     {
+//     console.log('ikkisi ham string 1');
+    
+// }
+// else if(typeof Name == typeof Number){
+//     console.log("ikkisi ham string 2");
+
+// }
+// else{
+
+//     console.log("Uchalasi ham bir vaqtda string emas");
+// }
+
+
+
+let a = 15
+let b = 176
+let c = 290
+
+if(a>b && a>c){
+
+    console.log(a);   
+    
+}else if(b>a && b>c){
+
+    console.log(b);
+    
+}else{
+    console.log(c);
+    
+}
+
